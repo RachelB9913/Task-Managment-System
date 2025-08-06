13e7b1e21e0d7833f405b794f6d58a1b65163873
Rachel <rachel.belokopytov@nuvocares.com>
2025-08-06 17:51:28 +0300

    small change

--- .history/src/main/java/com/rachel/taskManager/repository/ProjectRepository_20250806173052.java
+++ .history/src/main/java/com/rachel/taskManager/repository/ProjectRepository_20250806173052.java
@@ -3,7 +3,12 @@ package com.rachel.taskManager.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.rachel.taskManager.model.Project;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+
+    Page<Project> findAll(Pageable pageable);
 
 }
--- .history/src/main/java/com/rachel/taskManager/repository/ProjectRepository_20250806173108.java
+++ .history/src/main/java/com/rachel/taskManager/repository/ProjectRepository_20250806173108.java
@@ -3,6 +3,9 @@ package com.rachel.taskManager.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.rachel.taskManager.model.Project;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {
 
